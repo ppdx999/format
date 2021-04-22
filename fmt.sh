a0151fa07f5ce3bc19690349032bce3f01d2c36b
ppdx999 <ppdx999@gmail.com>
2021-04-22 18:05:52 +0900

    newly create bash format file.

--- fmt.sh
+++ fmt.sh
@@ -44,6 +44,45 @@ function error_exit(){
 	exit 1
 }
 
+# mktemp command doesn't comply with POSIX. This is the substitution.
+# source https://qiita.com/ko1nksm/items/45574a24ecd5f15a731e#%E3%81%99%E3%81%A7%E3%81%AB%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%81%8C%E3%81%82%E3%82%8B%E5%A0%B4%E5%90%88%E3%81%AE%E3%82%A8%E3%83%A9%E3%83%BC%E5%87%A6%E7%90%86
+make_tempfile() {
+	#--------------------------------------------------
+	#Usage:
+	#	make_tempfile [prefix] [suffix] [dir_path]
+	#
+	#Description:
+	#	Function for make a tmp file
+	#
+	#--------------------------------------------------
+  (
+    now=$(date +'%Y%m%d%H%M%S') || return $?
+    file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
+    umask 0077
+    set -C
+    : > "$file" || return $?
+    echo "$file"
+  )
+}
+
+make_tempdir() {
+	#--------------------------------------------------
+	#Usage:
+	#	make_tempdir [prefix] [suffix] [dir_path]
+	#
+	#Description:
+	#	Function for make a tmp directory
+	#
+	#--------------------------------------------------
+  (
+    now=$(date +'%Y%m%d%H%M%S') || return $?
+    file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
+    umask 0077
+    mkdir "$file" || return $?
+    echo "$file"
+  )
+}
+
 # Example of Arg check
 # n of args check
 #[ $# -eq 0 ] &&  error_exit "argument count" 
