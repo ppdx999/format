d460f770bdaa2ea59057076b8fbfebb014013186
ppdx999 <ppdx999@gmail.com>
2021-06-05 01:14:24 +0900

    Support for receiveing input from stdin

--- fmt.sh
+++ fmt.sh
@@ -15,76 +15,85 @@
 #	
 ###############################################################################
 
-# Debug mode
-#		-v : Prints shell input lines as they are read
-#		-x : Print command traces before executing command
-#set -xv
+_main(){
+}
 
-# Error Handling
-#		-e : when error happen, exit
-#		-o pipefail : even if error happen on the pipeline, exit
-#set -e -o pipefail
+init(){
+	:
+	# Debug mode
+	#		-v : Prints shell input lines as they are read
+	#		-x : Print command traces before executing command
+	#set -xv
 
-# Prepare for malicious rewriting of Environment variables
-set -u
-#umaks 0022
-PATH=/bin:/usr/bin:$HOME/bin
-IFS=$(printf ' \t\n_'); IFS={IFS%_}
-export IFS LC_ALL=C LANG=C PATH
+	# Error Handling
+	#		-e : when error happen, exit
+	#		-o pipefail : even if error happen on the pipeline, exit
+	#set -e -o pipefail
 
-# Error Exit Function
-function error_exit(){
-	#--------------------------------------------------
-	#Function for exit dut to fatal program error
-	# Accepts 1 argument:
-	#		string containing descriptive error message
-	#--------------------------------------------------
+	#prevent_malcious_env_var
+}
 
+prevent_malcious_env_var(){
+	set -u # If undefined variable appear, stop and exit
+	#umaks 0022
+	PATH=/bin:/usr/bin:$HOME/bin
+	IFS=$(printf ' \t\n_'); IFS={IFS%_}
+	export IFS LC_ALL=C LANG=C PATH
+}
+
+error_exit(){
 	echo "$0: ${1:-"Unknown Error"}" 1>&2
 	exit 1
 }
 
-# mktemp command doesn't comply with POSIX. This is the substitution.
-# source https://qiita.com/ko1nksm/items/45574a24ecd5f15a731e#%E3%81%99%E3%81%A7%E3%81%AB%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%81%8C%E3%81%82%E3%82%8B%E5%A0%B4%E5%90%88%E3%81%AE%E3%82%A8%E3%83%A9%E3%83%BC%E5%87%A6%E7%90%86
 make_tempfile() {
 	#--------------------------------------------------
 	#Usage:
 	#	make_tempfile [prefix] [suffix] [dir_path]
-	#
-	#Description:
-	#	Function for make a tmp file
-	#
 	#--------------------------------------------------
-  (
-    now=$(date +'%Y%m%d%H%M%S') || return $?
-    file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
-    umask 0077
-    set -C
-    : > "$file" || return $?
-    echo "$file"
-  )
+	(
+	now=$(date +'%Y%m%d%H%M%S') || return $?
+	file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
+	if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+		umask 0077
+	fi
+	set -C
+	: > "$file" || return $?
+	echo "$file"
+	)
 }
 
 make_tempdir() {
 	#--------------------------------------------------
 	#Usage:
 	#	make_tempdir [prefix] [suffix] [dir_path]
-	#
-	#Description:
-	#	Function for make a tmp directory
-	#
 	#--------------------------------------------------
-  (
-    now=$(date +'%Y%m%d%H%M%S') || return $?
-    file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
-    umask 0077
-    mkdir "$file" || return $?
-    echo "$file"
-  )
+	(
+	now=$(date +'%Y%m%d%H%M%S') || return $?
+	file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
+	if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+		umask 0077
+	fi
+	mkdir "$file" || return $?
+	echo "$file"
+	)
 }
 
-# Example of Arg check
-# n of args check
-#[ $# -eq 0 ] &&  error_exit "argument count" 
-# directory check
-#[ -d "$1" ] &&  error_exit "argument count" 
+arg_checks(){
+	[ $# -eq 0 ] && [ ! -p /dev/stdin ] &&  error_exit "Arg Error: no argument passed" 
+	#[ -d "$1" ] &&  error_exit "Error: ""$1"" doesn't exist or isn't directory" 
+}
+
+if [ -p /dev/stdin ]; then
+		# Take the entire standard output as input
+		#_main "$(cat -)"
+
+		# Receive line-by-line input
+		while read line
+		do
+				echo "$line"
+				_main "$@"
+		done
+else
+		_main "$@"
+fi 
