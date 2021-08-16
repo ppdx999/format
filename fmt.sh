1b79826d82d76829e3a64c2ef0d7333908c36de3
ppdx999 <ppdx999@gmail.com>
2021-08-16 22:08:05 +0900

    Add new feature: Option Parse On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
          modified:   fmt.sh

--- fmt.sh
+++ fmt.sh
@@ -32,11 +32,11 @@
 ### Knowledges notes ###
 
 # set options explanations
-# 	-v          : Prints shell input lines as they are read
-# 	-x          : Print command traces before executing command
-# 	-e          : when error happen, exit
-# 	-u          : If undefined variable appear, stop and exit
-# 	-o pipefail : even if error happen on the pipeline, exit
+#  -v          : Prints shell input lines as they are read
+#  -x          : Print command traces before executing command
+#  -e          : when error happen, exit
+#  -u          : If undefined variable appear, stop and exit
+#  -o pipefail : even if error happen on the pipeline, exit
 
 
 # example of argument checks
@@ -69,77 +69,146 @@
 # === Initialize shell environment ===================================
 set -eu
 if command -v umask &> /dev/null; then umask 0022; fi
-PATH='/bin:/usr/bin:$HOME/bin'
+export LC_ALL=C
+export PATH="$(command -p getconf PATH 2>/dev/null)${PATH+:}${PATH-}"
+case $PATH in :*) PATH=${PATH#?};; esac
+#export UNIX_STD=2003 # to make HP-UX conform to POSIX
 IFS=$(printf ' \t\n_'); IFS={IFS%_}
-export IFS LC_ALL=C LANG=C PATH
+export IFS
 
 # === Define the commonly used and useful functions ===================
 
 error_exit() {
-	echo "$0: ${1:-"Unknown Error"}" 1>&2
-	exit 1
+  echo "$0: ${1:-"Unknown Error"}" 1>&2
+  exit 1
 }
 
 print_usage_and_exit() {
   cat <<-USAGE 1>&2
-		Usage       : ${0##*/} [options] [XML_file]
-		Description :
-	USAGE
-  exit 1
+  Usage       : ${0##*/} [options] [XML_file]
+  Description :
+USAGE
+exit 1
 }
 
 detectOS() {
-   case "$(uname -s)" in
-   	Linux*)     echo Linux;;
-   	Darwin*)    echo Mac;;
-   	CYGWIN*)    echo Cygwin;;
-   	MINGW*)     echo MinGw;;
-   	*)          echo "UNKNOWN:$(uname -s)"
-   esac
+  case "$(uname -s)" in
+    Linux*)     echo Linux;;
+    Darwin*)    echo Mac;;
+    CYGWIN*)    echo Cygwin;;
+    MINGW*)     echo MinGw;;
+    *)          echo "UNKNOWN:$(uname -s)"
+  esac
 }
 
 cmd_exist() {
-	if command -v "$1" &> /dev/null; then
-		return 0
-	else
-		return 1
-	fi
+  if command -v "$1" &> /dev/null; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 
 make_tempfile() {
-	#--------------------------------------------------
-	#Usage:
-	#	make_tempfile [prefix] [suffix] [dir_path]
-	#--------------------------------------------------
-	(
-	now=$(date +'%Y%m%d%H%M%S') || return $?
-	file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
-	if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-		umask 0077
-	fi
-	set -C
-	: > "$file" || return $?
-	echo "$file"
-	)
+  #--------------------------------------------------
+  #Usage:
+  # make_tempfile [prefix] [suffix] [dir_path]
+  #--------------------------------------------------
+  (
+  now=$(date +'%Y%m%d%H%M%S') || return $?
+  file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
+  if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    umask 0077
+  fi
+  set -C
+  : > "$file" || return $?
+  echo "$file"
+)
 }
 
 make_tempdir() {
-	#--------------------------------------------------
-	#Usage:
-	#	make_tempdir [prefix] [suffix] [dir_path]
-	#--------------------------------------------------
-	(
-	now=$(date +'%Y%m%d%H%M%S') || return $?
-	file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
-	if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-		umask 0077
-	fi
-	mkdir "$file" || return $?
-	echo "$file"
-	)
+  #--------------------------------------------------
+  #Usage:
+  # make_tempdir [prefix] [suffix] [dir_path]
+  #--------------------------------------------------
+  (
+  now=$(date +'%Y%m%d%H%M%S') || return $?
+  file="${3:-${TMPDIR:-/tmp}}/${1:-}$now-$$${2:-}"
+  if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    umask 0077
+  fi
+  mkdir "$file" || return $?
+  echo "$file"
+)
 }
 
+######################################################################
+# Parse Arguments
+######################################################################
+
+# === Print the usage when "--help" is put ===========================
+case "$# ${1:-}" in
+  '1 -h'|'1 --help'|'1 --version') print_usage_and_exit;;
+esac
+
+# === Get the options, arguments and pipeline-input ===============================
+# --- initialize option parameters -----------------------------------
+#
+# some parameters here
+#
+# --- get them -------------------------------------------------------
+optmode=''
+while [ $# -gt 0 ]; do
+  case "$optmode" in
+    '') case "$1" in
+          --)       shift
+                    break
+                    ;;
+          -[hv]|--help|--version)
+                    print_usage_and_exit
+                    ;;
+          -[ndwl]*) ret=$(printf '%s\n' "${1#-}"                              |
+                          awk '{opt     = substr($0,1,1);                     #
+                                opt_str = (length($0)>1) ? substr($0,2) : ""; #
+                                printf("%s %s", opt, opt_str);              }')
+                    ret1=${ret%% *}
+                    ret2=${ret#* }
+                    case "$ret1$ret2" in
+                      n)  optmode='n'             ;;
+                      n*) n_param=$ret2;;
+                      d)  optmode='d'             ;;
+                      d*) d_param=$ret2              ;;
+                      w)  optmode='w'             ;;
+                      w*) w_param=$ret2  ;;
+                      l)  optmode='l'             ;;
+                      l*) l_param=$ret2 ;;
+                    esac
+                    ;;
+          -*)       print_usage_and_exit
+                    ;;
+          *)        case "$arg1" in '') arg1=$1; shift; continue ;; esac
+                    case "$arg2" in '') arg2=$1; shift; continue ;; esac
+                    case "$arg3" in '') arg3=$1; shift; continue ;; esac
+                    break
+                    ;;
+        esac
+        ;;
+    n)  n_param=$1
+        optmode=''
+        ;;
+    d)  d_param=$1
+        optmode=''
+        ;;
+    w)  w_param=$1
+        optmode=''
+        ;;
+    l)  l_param=$1
+        optmode=''
+        ;;
+  esac
+  shift
+done
 
 ###############################################################################
 # Main Routine 
@@ -147,18 +216,18 @@ make_tempdir() {
 
 _main() {
 
-	[ $# -eq 0 ] && print_usage_and_exit
+  [ $# -eq 0 ] && print_usage_and_exit
 }
 
 if [ -p /dev/stdin ]; then
-		# Take the entire standard output as input
-		#_main "$(cat -)"
+  # Take the entire standard output as input
+  #_main "$(cat -)"
 
-		# Receive line-by-line input
-		while read -r line
-		do
-				_main $line 
-		done
+  # Receive line-by-line input
+  while read -r line
+  do
+    _main $line 
+  done
 else
-		_main "$@"
+  _main "$@"
 fi 
